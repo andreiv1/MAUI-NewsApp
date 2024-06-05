ebd1b14c27c104493157085613b84093bed5d8cf
Andrei Vas <114833527+andreiv1@users.noreply.github.com>
2024-06-05 13:23:52 +0300

    News service

---
+++ MAUI-NewsApp.Data/Services/INewsService.cs
@@ -0,0 +1,18 @@
+﻿using MAUI_NewsApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_NewsApp.Data.Services
+{
+    public interface INewsService
+    {
+        public ICollection<string> GetKeywords();
+        public ICollection<Category> GetCategories();
+        public ICollection<Article> GetLatestArticles();
+        public ICollection<Article> GetArticlesByCategory(string category);
+        public ICollection<Article> GetArticlesByTag(string tag);
+    }
+}
