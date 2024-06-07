d66d58d318b56cd94b45b0f4276618b4a1c542db
Andrei Vas <114833527+andreiv1@users.noreply.github.com>
2024-06-08 01:31:42 +0300

    Added UI converters

---
+++ MAUI-NewsApp.UI/Converters/ListToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_NewsApp.UI.Converters
+{
+    public class ListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is List<string> list)
+            {
+                return string.Join(", ", list.Select(x => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(x.ToLower())));
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Split(new[] { ", " }, StringSplitOptions.None).ToList();
+            }
+            return new List<string>();
+        }
+    }
+}
---
+++ MAUI-NewsApp.UI/Converters/RelativeTimeConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_NewsApp.UI.Converters
+{
+    internal class RelativeTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime date)
+            {
+                TimeSpan difference = DateTime.Now - date;
+                if(difference.TotalMinutes < 1)
+                {
+                    return "just now";
+                }
+                else if(difference.TotalMinutes < 60)
+                {
+                    return $"{difference.Minutes}m ago";
+                }
+                else if(difference.TotalHours < 24)
+                {
+                    return $"{difference.Hours}h ago";
+                }
+                else if(difference.TotalDays < 7)
+                {
+                    return $"{difference.Days}d ago";
+                }
+                else if(difference.TotalDays < 30)
+                {
+                    return $"{difference.Days / 7}w ago";
+                }
+                else if(difference.TotalDays < 365)
+                {
+                    return $"{difference.Days / 30}mo ago";
+                }
+                else
+                {
+                    return $"{difference.Days / 365}y ago";
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
