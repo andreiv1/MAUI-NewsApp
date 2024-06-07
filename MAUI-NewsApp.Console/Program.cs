5406dfb8488537ff27596c5ffa3ba0032e52d553
Andrei Vas <114833527+andreiv1@users.noreply.github.com>
2024-06-08 01:30:42 +0300

    Testing console app

--- MAUI-NewsApp.Console/Program.cs
+++ MAUI-NewsApp.Console/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace MAUI_NewsApp.Console
+﻿using MAUI_NewsApp.Data.Services;
+
+namespace MAUI_NewsApp.Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+           INewsService newsService = new NewsDataIoService();
+
+           var articles = newsService.GetLatestArticles().Result;
+
+           foreach(var article in articles)
+            {
+               System.Console.WriteLine(article.Title);
+           }
         }
     }
 }
